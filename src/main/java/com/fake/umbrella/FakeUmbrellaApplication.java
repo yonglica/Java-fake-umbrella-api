5446361f380a88f0a3d32acdb25560230714a056
Yong Li <liyong2005@gmail.com>
2021-02-04 22:27:13 -0500

    init commit

---
+++ src/main/java/com/fake/umbrella/FakeUmbrellaApplication.java
@@ -0,0 +1,13 @@
+package com.fake.umbrella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeUmbrellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeUmbrellaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fake/umbrella/FakeUmbrellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fake.umbrella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeUmbrellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
